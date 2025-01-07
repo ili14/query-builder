738c0d15bcaee279ecad6da5f6f5067f03d80d3b
ili14 <ilidevloper@gmail.com>
2025-01-07 18:37:19 +0330

    1403/10/18 - 18:37

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css'
-import React, { useCallback, useMemo, useState } from 'react'
+import './App.css';
+import React, { useCallback, useMemo, useState } from 'react';
 import {
     Box,
+    Button,
     Card,
     Grid2 as Grid,
     List,
@@ -11,19 +12,20 @@ import {
     ListItemText,
     ListSubheader,
     Typography,
-} from '@mui/material'
-import { PiFileSqlFill } from 'react-icons/pi'
-import { FaTable } from 'react-icons/fa'
-import { Editor } from '@monaco-editor/react'
-import DiagramSection from './DiagramSection'
-import SelectedTableSection from './SelectedTableSection'
-import QueryChangerSection, { RelationshipForQuery } from './QueryChangerSection'
-import { Context } from 'react-zoom-pan-pinch'
+} from '@mui/material';
+import { PiFileSqlFill } from 'react-icons/pi';
+import { FaTable } from 'react-icons/fa';
+import { Editor } from '@monaco-editor/react';
+import DiagramSection from './DiagramSection';
+import SelectedTableSection from './SelectedTableSection';
+import QueryChangerSection, { RelationshipForQuery } from './QueryChangerSection';
 import {
     CheckEntireTableColumnsHandler,
     CheckSingleColumnHandler,
     EntireDiagramSectionContext,
-} from './Contexts/DiagramSectionContext'
+} from './Contexts/DiagramSectionContext';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import selectedTableSection from './SelectedTableSection';
 
 // ! this array is simulate of table with columns comes from server
 const listOfTablesWithColumns: TableWithColumns[] = [
@@ -33,7 +35,7 @@ const listOfTablesWithColumns: TableWithColumns[] = [
         columns: [
             { name: 'id' },
             { name: 'name' },
-            { name: 'email' }, // example additional column
+            { name: 'email', description: 'this is a email of user' }, // example additional column
             { name: 'createdAt' }, // example additional column
         ],
     },
@@ -133,7 +135,7 @@ const listOfTablesWithColumns: TableWithColumns[] = [
             { name: 'productId', relationship: [{ targetColumn: 'id', targetTable: 'products' }] }, // productId references products table
         ],
     },
-]
+];
 
 // ! this array is simulate of tables without columns comes from server
 const listOfTables: Table[] = [
@@ -181,25 +183,25 @@ const listOfTables: Table[] = [
         id: 4,
         name: 'wishlists',
     },
-]
+];
 
 
 function App(): JSX.Element {
-    const [tables, setTables] = useState<Table[]>(listOfTables)
-    const [diagramsTable, setDiagramsTable] = useState<TableWithColumns[]>([])
-    const [lastTableWithColumnsSelected, setLastTableWithColumnsSelected] = useState<Table | null>(null)
-    const [builtQuery, setbuiltQuery] = useState<string>('')
+    const [tables, setTables] = useState<Table[]>(listOfTables);
+    const [diagramsTable, setDiagramsTable] = useState<TableWithColumns[]>([]);
+    const [lastTableWithColumnsSelected, setLastTableWithColumnsSelected] = useState<TableWithColumns | null>(null);
+    const [builtQuery, setbuiltQuery] = useState<string>('');
 
-    const draggingTableWithColumnsRef = React.useRef<TableWithColumns>(undefined)
+    const draggingTableWithColumnsRef = React.useRef<TableWithColumns>(undefined);
 
     const queryingColumnsTables = useMemo(() => {
         return diagramsTable.map((table) => {
-            return { ...table, name: table.name, columns: table?.columns?.filter((column) => column.checked) }
-        })
-    }, [diagramsTable])
+            return { ...table, name: table.name, columns: table?.columns?.filter((column) => column.checked) };
+        });
+    }, [diagramsTable]);
 
     const relationshipForQueries: RelationshipForQuery[] = useMemo(() => {
-        let result: RelationshipForQuery[] = []
+        let result: RelationshipForQuery[] = [];
         diagramsTable.forEach((table) => {
             return table.columns?.forEach((column) => {
                 column.relationship?.forEach((relationship) => {
@@ -209,85 +211,85 @@ function App(): JSX.Element {
                             targetTable: relationship.targetTable,
                             sourceTable: table.name,
                             sourceColumn: column.name,
-                        })
-                })
-            })
-        })
-        return result
-    }, [diagramsTable])
+                        });
+                });
+            });
+        });
+        return result;
+    }, [diagramsTable]);
 
     const handleCheckSingleColumn: CheckSingleColumnHandler = useCallback((tableName, columnName, checked) => {
-        console.log('checked', tableName, columnName, checked)
+        console.log('checked', tableName, columnName, checked);
         setDiagramsTable(() => {
             const newTables = diagramsTable.map((table) => {
                 if (table.name === tableName) {
                     const newColumns = table?.columns?.map((column) => {
                         if (column.name === columnName) {
-                            return { ...column, checked }
+                            return { ...column, checked };
                         }
-                        return column
-                    })
-                    return { ...table, columns: newColumns }
+                        return column;
+                    });
+                    return { ...table, columns: newColumns };
                 }
-                return table
-            })
-            return newTables
-        })
-    }, [diagramsTable])
+                return table;
+            });
+            return newTables;
+        });
+    }, [diagramsTable]);
 
     const handleCheckEntireTableColumns: CheckEntireTableColumnsHandler = useCallback((tableName, checked) => {
-        console.log('checked', tableName, checked)
+        console.log('checked', tableName, checked);
         setDiagramsTable(() => {
             const newTables = diagramsTable.map((table) => {
                 if (table.name === tableName) {
                     const newColumns = table?.columns?.map((column) => {
 
-                        return { ...column, checked }
+                        return { ...column, checked };
 
-                    })
-                    return { ...table, columns: newColumns }
+                    });
+                    return { ...table, columns: newColumns };
                 }
-                return table
-            })
-            return newTables
-        })
-    }, [diagramsTable])
+                return table;
+            });
+            return newTables;
+        });
+    }, [diagramsTable]);
 
     const handleQueryChange = useCallback((query: string) => {
-        setbuiltQuery(query)
-    }, [])
+        setbuiltQuery(query);
+    }, []);
 
     const getTableWithColumns = useCallback((table: Table) => {
         // Todo replace bellow line must be replaced with api call and get Table from server
-        const newTableWithColumns = listOfTablesWithColumns.find((t) => t.name === table.name)
-        return newTableWithColumns
-    }, [])
+        const newTableWithColumns = listOfTablesWithColumns.find((t) => t.name === table.name);
+        return newTableWithColumns;
+    }, []);
 
     const updateDiagramWithTableWithColumns = useCallback((tableWithColumns?: TableWithColumns) => {
         if (tableWithColumns) {
-            setLastTableWithColumnsSelected(tableWithColumns)
+            setLastTableWithColumnsSelected(tableWithColumns);
             if (diagramsTable.some((t) => t.name === tableWithColumns.name)) {
-                const newTables = diagramsTable.filter((t) => t.name !== tableWithColumns.name)
-                setDiagramsTable(newTables)
+                const newTables = diagramsTable.filter((t) => t.name !== tableWithColumns.name);
+                setDiagramsTable(newTables);
             } else {
-                const newTables = [...diagramsTable, tableWithColumns]
-                setDiagramsTable(newTables)
+                const newTables = [...diagramsTable, tableWithColumns];
+                setDiagramsTable(newTables);
             }
         } else {
-            alert('table not found')
+            alert('table not found');
         }
-    }, [diagramsTable])
+    }, [diagramsTable]);
 
     const handleTablesSectionDragStart = useCallback((table: Table) => {
-        const newTableWithColumns = getTableWithColumns(table)
+        const newTableWithColumns = getTableWithColumns(table);
         if (newTableWithColumns)
-            draggingTableWithColumnsRef.current = newTableWithColumns
-    }, [getTableWithColumns])
+            draggingTableWithColumnsRef.current = newTableWithColumns;
+    }, [getTableWithColumns]);
 
     const handleDiagramSectionDrop = useCallback(() => {
         if (draggingTableWithColumnsRef?.current)
-            updateDiagramWithTableWithColumns(draggingTableWithColumnsRef.current)
-    }, [updateDiagramWithTableWithColumns])
+            updateDiagramWithTableWithColumns(draggingTableWithColumnsRef.current);
+    }, [updateDiagramWithTableWithColumns]);
 
     return (
         <Box>
@@ -301,42 +303,49 @@ function App(): JSX.Element {
                  className="[&_.cell]:bg-red-500 [&_.cell]:rounded-xl h-[calc(100vh_-_60px)] flex flex-col "
             >
                 <Grid container spacing={2} height={'60%'}>
-                    <Grid className="cell h-full" size={3}>
-                        <Card variant={'outlined'} className="w-full h-full overflow-hidden">
-                            <TableListSection
-                                tables={tables}
-                                onClickTable={(table) => {
-                                    const newTableWithColumns = listOfTablesWithColumns.find((t) => t.name === table.name)
-                                    if (newTableWithColumns) {
-                                        setLastTableWithColumnsSelected(newTableWithColumns)
-                                    }
-                                }}
-                                onDragItemStart={handleTablesSectionDragStart}
-                                onDoubleClickTable={(table) => {
-                                    const newTableWithColumns = getTableWithColumns(table)
-                                    if (newTableWithColumns) {
-                                        setLastTableWithColumnsSelected(newTableWithColumns)
-                                        updateDiagramWithTableWithColumns(newTableWithColumns)
-                                    }
-                                }} />
-                        </Card>
-                    </Grid>
-                    <Grid className="cell" size={6}>
-                        <Card variant={'outlined'} className="w-full h-full">
-                            <EntireDiagramSectionContext.Provider
-                                value={{
-                                    onCheckSingleColumn: handleCheckSingleColumn,
-                                    onCheckEntireTableColumns: handleCheckEntireTableColumns,
-                                }}>
-                                <DiagramSection onDrop={handleDiagramSectionDrop} tables={diagramsTable} />
-                            </EntireDiagramSectionContext.Provider>
-                        </Card>
-                    </Grid>
-                    <Grid className="cell" size={3}>
-                        <Card variant={'outlined'} className="w-full h-full"><MyEditor value2={builtQuery} /></Card>
-                    </Grid>
+                    <PanelGroup direction="horizontal" className="gap-1">
+                        <Panel defaultSize={10} minSize={1}>
+                            <Card variant={'outlined'} className="w-full h-full overflow-hidden">
+                                <TableListSection
+                                    tables={tables}
+                                    selectedTable={lastTableWithColumnsSelected ?? undefined}
+                                    onClickTable={(table) => {
+                                        const newTableWithColumns = listOfTablesWithColumns.find((t) => t.name === table.name);
+                                        if (newTableWithColumns) {
+                                            setLastTableWithColumnsSelected(newTableWithColumns);
+                                        }
+                                    }}
+                                    onDragItemStart={handleTablesSectionDragStart}
+                                    onDoubleClickTable={(table) => {
+                                        const newTableWithColumns = getTableWithColumns(table);
+                                        if (newTableWithColumns) {
+                                            setLastTableWithColumnsSelected(newTableWithColumns);
+                                            updateDiagramWithTableWithColumns(newTableWithColumns);
+                                        }
+                                    }} />
+                            </Card>
+                        </Panel>
+                        <PanelResizeHandle />
+                        <Panel minSize={30}>
+                            <Card variant={'outlined'} className="w-full h-full">
+                                <EntireDiagramSectionContext.Provider
+                                    value={{
+                                        onCheckSingleColumn: handleCheckSingleColumn,
+                                        onCheckEntireTableColumns: handleCheckEntireTableColumns,
+                                    }}>
+                                    <DiagramSection onDrop={handleDiagramSectionDrop} tables={diagramsTable} />
+                                </EntireDiagramSectionContext.Provider>
+                            </Card>
+                        </Panel>
+                        <PanelResizeHandle />
+                        <Panel defaultSize={30} minSize={1} className="h-full">
+                            <Card variant={'outlined'} className="w-full h-full"><MyEditor
+                                value2={builtQuery} />
+                            </Card>
+                        </Panel>
+                    </PanelGroup>
                 </Grid>
-                <Grid container spacing={2} height={'39%'}>
+                <Grid container spacing={2} height={'32%'}>
                     <Grid className="cell h-full" size={3}>
                         <Card variant={'outlined'} className="w-full h-full"><SelectedTableSection
                             tableWithColumns={lastTableWithColumnsSelected} /></Card>
@@ -349,9 +358,22 @@ function App(): JSX.Element {
                                 tablesWithColumns={queryingColumnsTables} /></Card>
                     </Grid>
                 </Grid>
+                <Box className="flex justify-between">
+                    <Button variant="contained">
+                        show result
+                    </Button>
+                    <Box display="flex" gap={1}>
+                        <Button variant="contained">
+                            ok
+                        </Button>
+                        <Button variant="contained">
+                            cancel
+                        </Button>
+                    </Box>
+                </Box>
             </Box>
         </Box>
-    )
+    );
 }
 
 interface Table {
@@ -360,13 +382,14 @@ interface Table {
 }
 
 export interface TableWithColumns extends Table {
-    columns?: Column[]
+    columns?: Column[];
 }
 
 export type Column = {
     name: string,
     type?: string,
     checked?: boolean,
+    description?: string,
     relationship?: {
         targetTable: string,
         targetColumn: string
@@ -380,9 +403,26 @@ interface TableListSectionProps {
     onDoubleClickTable?: (table: Table) => void;
     onDragItemStart?: (table: Table) => void;
     tables?: Table[];
+    selectedTable?: TableWithColumns;
 }
 
-function TableListSection({ onClickTable, onDoubleClickTable, onDragItemStart, tables }: TableListSectionProps) {
+function TableListSection({
+                              onClickTable,
+                              onDoubleClickTable,
+                              onDragItemStart,
+                              tables,
+                              selectedTable,
+                          }: TableListSectionProps) {
+    const relationWithTables = useMemo(() => {
+        const result: string[] = [];
+        selectedTable?.columns?.forEach((column) => {
+            column.relationship?.forEach((rel) => {
+                if (!result.includes(rel.targetTable))
+                    result.push(rel.targetTable);
+            });
+        });
+        return result;
+    }, [selectedTable]);
     return <>
         <List sx={{ maxHeight: '100%' }} className={'!overflow-auto'} subheader={
             <ListSubheader component="div" id="nested-list-subheader">
@@ -390,21 +430,21 @@ function TableListSection({ onClickTable, onDoubleClickTable, onDragItemStart, t
             </ListSubheader>
         }>
             {tables?.map((table, index) => {
-
+                const haveRelationshipWithTable = relationWithTables.includes(table.name);
                 return (
                     <ListItem
                         key={index}
                         onDragStart={() => {
                             if (onDragItemStart)
-                                onDragItemStart(table)
+                                onDragItemStart(table);
                         }}
                         onDoubleClick={() => {
                             if (onDoubleClickTable)
-                                onDoubleClickTable(table)
+                                onDoubleClickTable(table);
                         }}
                         onClick={() => {
                             if (onClickTable)
-                                onClickTable(table)
+                                onClickTable(table);
                         }}
                         draggable
                         disablePadding
@@ -413,28 +453,29 @@ function TableListSection({ onClickTable, onDoubleClickTable, onDragItemStart, t
                             <ListItemIcon className="!text-blue-500 ">
                                 <FaTable />
                             </ListItemIcon>
-                            <ListItemText primary={table.name} />
+                            <ListItemText className={haveRelationshipWithTable ? 'font-bold text-blue-500' : ''}
+                                          primary={table.name} />
                         </ListItemButton>
                         <div className="h-[1px] bg-blue-500 absolute bottom-0 left-4 right-4 opacity-20 line"></div>
                     </ListItem>
-                )
+                );
             })}
 
         </List>
-    </>
+    </>;
 
 }
 
 interface MyEditorProps {
-    value2?: string
+    value2?: string;
 }
 
 function MyEditor({ value2 }: MyEditorProps) {
-    const [code, setCode] = useState<string | undefined>('SELECT * FROM users WHERE id > 10 AND email LIKE "%example.com%"')
+    const [code, setCode] = useState<string | undefined>('SELECT * FROM users WHERE id > 10 AND email LIKE "%example.com%"');
     return <Box height={'100%'}>
         <Editor className="h-full" options={{ wordWrap: 'on' }}
                 defaultLanguage="sql" defaultValue="// some comment" value={value2} />
-    </Box>
+    </Box>;
 }
 
-export default App
+export default App;
--- src/SelectedTableSection/index.tsx
+++ src/SelectedTableSection/index.tsx
@@ -1,27 +1,28 @@
 import React from 'react'
-import {Box, Divider, Paper, Typography} from "@mui/material";
-import {TableWithColumns} from "../App";
+import { Box, Divider, Paper, Typography } from '@mui/material'
+import { TableWithColumns } from '../App'
 
 interface SelectedTablesProps {
     tableWithColumns: TableWithColumns | null;
 }
 
-const Index: React.FC<SelectedTablesProps> = ({tableWithColumns}) => {
+const Index: React.FC<SelectedTablesProps> = ({ tableWithColumns }) => {
     return (
         <Paper elevation={0}>
-            <Box className='bg-blue-100'>
-                <Typography sx={{p: 1}} variant={'h6'} fontSize={14} color={"gray"}>
+            <Box className="bg-blue-100">
+                <Typography sx={{ p: 1 }} variant={'h6'} fontSize={14} color={'gray'}>
                     Columns of <b className={'font-bold text-blue-400'}>{tableWithColumns?.name}</b> Table
                 </Typography>
-                <Divider/>
+                <Divider />
             </Box>
             <Box className="flex flex-col gap-1 pt-1 p-2">
                 {tableWithColumns?.columns?.map((column, index) => (
                     <>
-                        <Box>
-                            {column.name}
+                        <Box display="flex" justifyContent="space-between">
+                            {column.name} {column.description ? <><Divider  flexItem orientation="vertical" />
+                            <Box>{column.description}</Box></> : undefined}
                         </Box>
-                        <Divider className='last:hidden' />
+                        <Divider className="last:hidden" />
                     </>
                 ))}
                 {!tableWithColumns && <Box className={'animate-pulse'}>No table selected</Box>}
